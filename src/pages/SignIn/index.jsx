81717c3a56829be458575241a051b2700d4e48b7
Ygor Bontia <ygorbontiaa@gmail.com>
2023-04-20 15:30:16 -0300

    create SignIn page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,11 +1,25 @@
 import { SignInSC } from './style';
 
+import { Brand } from '../../components/Brand';
 import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 
 export function SignIn() {
   return (
     <SignInSC>
-      <Input label="Email" type="email" placeholder="Exemplo: exemplo@exemplo.com.br" />
+      <main>
+        <Brand />
+        
+        <form>
+          <Input label="Email" type="email" placeholder="Exemplo: exemplo@exemplo.com.br" />
+
+          <Input label="Senha" type="password" placeholder="No mínimo 6 caracteres" />
+
+          <Button type="button" title="Entrar" />
+
+          <a href="#">Criar uma conta</a>
+        </form>
+      </main>
     </SignInSC>
   )
 };
--- src/pages/SignIn/style.js
+++ src/pages/SignIn/style.js
@@ -1,5 +1,33 @@
 import styled from 'styled-components';
 
 export const SignInSC = styled.div`
+  width: 100%;
+  height: 100vh;
+  
+    display: grid;
+    place-content: center;
+  
+  main {
+    width: 31.6rem;
 
+    > div {
+      margin-bottom: 7.3rem;
+    }
+
+    > form {
+      display: flex;
+      flex-direction: column;
+      gap: 3.2rem;
+  
+      a {
+        margin: 0 auto;
+  
+        font-family: ${({ theme }) => theme.fonts.poppins.medium_100.family};
+        font-weight: ${({ theme }) => theme.fonts.poppins.medium_100.weight};
+        font-size: ${({ theme }) => theme.fonts.poppins.medium_100.size};
+        line-height: ${({ theme }) => theme.fonts.poppins.medium_100.height};
+        color: ${({ theme }) => theme.colors.light_100};
+      }
+    }
+  }
 `;
