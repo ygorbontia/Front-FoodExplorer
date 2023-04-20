9b343c18aa530715c6c23e69d30482147c9d0c68
Ygor Bontia <ygorbontiaa@gmail.com>
2023-04-20 15:34:05 -0300

    create SignUp page

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,27 @@
+import { SignUpSC } from './style';
+
+import { Brand } from '../../components/Brand';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+export function SignUp() {
+  return (
+    <SignUpSC>
+      <main>
+        <Brand />
+        
+        <form>
+          <Input label="Seu nome" type="text" placeholder="Exemplo: exemplo@exemplo.com.br" />
+
+          <Input label="Email" type="email" placeholder="Exemplo: exemplo@exemplo.com.br" />
+
+          <Input label="Senha" type="password" placeholder="No mínimo 6 caracteres" />
+
+          <Button type="button" title="Criar conta" />
+
+          <a href="#">Já tenho uma conta</a>
+        </form>
+      </main>
+    </SignUpSC>
+  )
+};
