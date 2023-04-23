384967ee4edced2400b04349db2540409801d058
Ygor Bontia <ygorbontiaa@gmail.com>
2023-04-23 15:58:32 -0300

    create Dish page

---
+++ src/pages/Dish/index.jsx
@@ -0,0 +1,14 @@
+import { DishSC } from './style';
+
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
+
+export function Dish() {
+  return (
+    <DishSC>
+      <Header />
+
+      <Footer />
+    </DishSC>
+  )
+}
