89eb6573e5b60c85cb924960cfb532d4f8b90194
Ygor Bontia <ygorbontiaa@gmail.com>
2023-04-23 15:58:49 -0300

    update App page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import theme from './styles/theme';
 
-import { Home } from './pages/Home';
+import { Dish } from './pages/Dish';
 
 export function App() {
   return (
     <ThemeProvider theme={ theme }>
       <GlobalStyle />
-      <Home />
+      <Dish />
     </ThemeProvider>
   )
 };
