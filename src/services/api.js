b22eb406ddc3b2dbebdcef4dbcba809248d225c5
Ygor Bontia <ygorbontiaa@gmail.com>
2023-05-04 21:13:32 -0300

    update baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: "https://foodexplorer-api-9l6k.onrender.com"
-  baseURL: "http://localhost:3000"
+  baseURL: "https://foodexplorer-api-9l6k.onrender.com"
+  // baseURL: "http://localhost:3000"
 }) 
